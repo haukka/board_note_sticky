01d949f4088b7cfa2c85e4611ed1eea57fba24a4
moss <mossanthony4@gmail.com>
2016-03-19 19:42:06 +0100

    Creation of the sticky note board

---
+++ first_version/app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var Note = [];
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+server = require('http').createServer(app),
+io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('getData', {data: Note});
+
+    socket.on('NoteCreation', function(data) {
+	Note.push(data);
+	socket.broadcast.emit('CreateNote', data);
+    });
+
+    socket.on('NoteUpdate', function(data) {
+	var item = Note.map(function(e) {return e.id; }).indexOf(data.id);
+	Note[item] = data;
+	socket.broadcast.emit('UpdateNote', data);
+    });
+
+    socket.on('NoteDrag', function(data){
+	var item = Note.map(function(e) {return e.id; }).indexOf(data.id);
+	Note[item].x = data.x;
+	Note[item].y = data.y;
+	socket.broadcast.emit('DragNote', data);
+    });
+
+    socket.on('NoteRemove', function(data){
+	var item = Note.map(function(e) {return e.id; }).indexOf(data.id);
+	Note.splice(item, 1);
+	socket.broadcast.emit('RemoveNote', data);
+    });
+});
+
+server.listen(3000);
